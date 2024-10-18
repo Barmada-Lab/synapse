feb8971e1ec70e96ebbdea72b474b5de023daf01
Jacob Waksmacki <jwaksmac@umich.edu>
2024-10-18 11:01:02 -0400

    mute tests of disabled behavior

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -37,13 +37,13 @@ test("Log In button is visible", async ({ page }) => {
   await expect(page.getByRole("button", { name: "Log In" })).toBeVisible()
 })
 
-test("Forgot Password link is visible", async ({ page }) => {
-  await page.goto("/login")
+// test("Forgot Password link is visible", async ({ page }) => {
+//   await page.goto("/login")
 
-  await expect(
-    page.getByRole("link", { name: "Forgot password?" }),
-  ).toBeVisible()
-})
+//   await expect(
+//     page.getByRole("link", { name: "Forgot password?" }),
+//   ).toBeVisible()
+// })
 
 test("Log in with valid email and password ", async ({ page }) => {
   await page.goto("/login")
