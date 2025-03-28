3be84ace276920b92e71f77c9bdcc08827f45a32
Jacob Waksmacki <jwaksmac@umich.edu>
2025-03-28 18:27:13 +0900

    Add some utility functions to supply some of synapse's functionality offline

--- backend/app/core/cli.py
+++ backend/app/core/cli.py
@@ -3,10 +3,14 @@ from datetime import datetime, timedelta
 from pathlib import Path
 
 import pandas as pd
+from globus_compute_sdk import Executor
 from typer import Typer
 
+from app.acquisition.flows.analysis import submit_sbatch_job
+from app.acquisition.flows.artifact_collections import _sync_cmd
 from app.acquisition.flows.overlord import BatchParams, write_batch_xml
 from app.acquisition.models import Location
+from app.core.config import settings
 
 app = Typer()
 
@@ -46,12 +50,52 @@ def _parse_batch_params(
         )
 
 
-@app.command()
+@app.command(
+    help=(
+        "Dump Overlord XML files given a CSV conforming to the"
+        "create_acquisition_plan format"
+    )
+)
 def dump_xmls(csv_path: Path, output_dir: Path = Path(".")):
     df = pd.read_csv(csv_path).fillna("")
     for _, row in df.iterrows():
         for params in _parse_batch_params(row, output_dir):
             write_batch_xml(params)
+
+
+@app.command(
+    help=(
+        "Sync acquisitions to the configured ANALYSIS_DIR, given a CSV file"
+        "conforming to the create_analysis_plan format"
+    )
+)
+def sync_experiments(csv_path: Path):
+    df = pd.read_csv(csv_path).fillna("")
+    orig = settings.ACQUISITION_DIR
+    dest = settings.ANALYSIS_DIR
+    for _, row in df.iterrows():
+        acquisition_name = row["acquisition_name"]
+        if not (orig / acquisition_name).exists():
+            print(f"Acquisition {acquisition_name} not found")
+            continue
+        print(f"Syncing {acquisition_name} to {dest}")
+        _sync_cmd(orig / acquisition_name, dest)
+
+
+@app.command(
+    help=(
+        "Run analyses on the cluster given a CSV file conforming to the"
+        "create_analysis_plan format. Requires the experiment to be present in"
+        "ANALYSIS_DIR first."
+    )
+)
+def run_analyses(csv_path: Path):
+    df = pd.read_csv(csv_path).fillna("")
+    with Executor(endpoint_id=settings.GLOBUS_ENDPOINT_ID) as executor:
+        for _, row in df.iterrows():
+            sbatch_args = [row["analysis_cmd"], *row["analysis_args"].split(",")]
+            print(f"Submitting {sbatch_args} to the cluster")
+            submit_sbatch_job(sbatch_args, executor)
 
 
 @app.command(hidden=True)
